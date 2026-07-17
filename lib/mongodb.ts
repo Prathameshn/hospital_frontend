7e7d8c132fd8afe9f1c9ebc37363d1b779c967f7
Prathmesh Naik <prathmeshn007@gmail.com>
2026-07-17 16:25:56 +0530

    test url

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,6 +4,7 @@ const MONGODB_URI = process.env.MONGODB_URI!;
 
 export async function connectDB() {
   try {
+    console.log("MongoDB URI:", MONGODB_URI);
     await mongoose.connect(MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (error) {
